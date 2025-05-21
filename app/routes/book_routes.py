be9f905559917a18beb92d6ef9a23185897bea17
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2025-05-21 16:11:48 -0300

    rota get book por id

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -4,3 +4,4 @@ from app.controller import book_controller
 book_bp = Blueprint("books", __name__)  
 book_bp.route("/books", methods=["POST"])(book_controller.add_book)
 book_bp.route("/books", methods=["GET"])(book_controller.get_books)
+book_bp.route("/books/<int:book_id>", methods=["GET"])(book_controller.get_book)
