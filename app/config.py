ad7923d91daf927ac1d153f13aaeae641e1f032c
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2025-05-28 16:52:52 -0300

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -2,3 +2,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///projeto.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+
+class TestingConfig(Config):
+    TESTING = True 
+    SQLALCHEMY_DATABASE_URI = "sqlite:///memory:"
